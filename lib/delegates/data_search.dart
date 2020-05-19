0d1b3db6ef3e62cb6b5abe7badfcaaa5a553aa41
ElynekerLuciani <elyneker.sax@hotmail.com>
2020-05-19 19:02:27 -0300

    finalizando sistema de busca

--- lib/delegates/data_search.dart
+++ lib/delegates/data_search.dart
@@ -1,3 +1,5 @@
+import 'dart:convert';
+
 import 'package:flutter/material.dart';
 import 'package:http/http.dart' as http;
 
@@ -33,22 +35,65 @@ class DataSearch extends SearchDelegate<String> {
 
   @override
   Widget buildResults(BuildContext context) {
+    Future.delayed(Duration.zero).then((_) => close(context, query));
     return Container(
 
     );
   }
 
+  //carrega toda vez que digita na pesquisa
   @override
   Widget buildSuggestions(BuildContext context) {
-    return Container(
+    if(query.isEmpty){
+      return Container(
 
-    );
+      );
+    } else {
+      return FutureBuilder<List>(
+        future: suggestions(query),
+        // ignore: missing_return
+        builder: (context, snapshot){
+          if(! snapshot.hasData) {
+            return Center(
+              child: CircularProgressIndicator(),
+            );
+          } else {
+            return ListView.builder(
+                // ignore: missing_return
+                itemBuilder: (context, index) {
+                  return ListTile(
+                    title: Text(snapshot.data[index]),
+                    leading: Icon(Icons.play_arrow),
+                    onTap: (){
+                      close(context, snapshot.data[index]);
+
+                    },
+                  );
+
+                },
+              itemCount: snapshot.data.length,
+            );
+          }
+        },
+      );
+    }
+
   }
 
-  suggestions(String search) async {
+  Future<List> suggestions(String search) async {
     http.Response response = await http.get(
         "http://suggestqueries.google.com/complete/search?hl=en&ds=yt&client=youtube&hjson=t&cp=1&q=$search&format=5&alt=json"
     );
+
+    if(response.statusCode == 200) {
+      return json.decode(response.body)[1].map((doc){
+        return doc[0];
+
+      }).toList();
+
+    } else {
+      throw Exception("Falha ao carregar sugestões");
+    }
   }
   
 
