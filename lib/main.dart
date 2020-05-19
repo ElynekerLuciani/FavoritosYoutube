5d646a607dfdac06452fb66cfed8f78a0b6ddd71
ElynekerLuciani <elyneker.sax@hotmail.com>
2020-05-19 17:59:29 -0300

    criando layput da tela home

--- lib/main.dart
+++ lib/main.dart
@@ -2,12 +2,12 @@ import 'package:favoritosyoutube/api/api.dart';
 import 'package:favoritosyoutube/widgets/app.dart';
 import 'package:flutter/material.dart';
 
-//void main() => runApp(App());
-
-void main() {
-  Api api = Api();
-  api.search("eletronica");
-    
-  runApp(App());
-}
+void main() => runApp(App());
+//
+//void main() {
+//  Api api = Api();
+//  api.search("eletronica");
+//
+//  runApp(App());
+//}
 
--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -3,6 +3,39 @@ import 'package:flutter/material.dart';
 class HomeScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Scaffold(
+      appBar: AppBar(
+        title: Container(
+          height: 25.0,
+          child: Image.asset("images/youtube.png"),
+        ),
+        elevation: 1.0,
+        backgroundColor: Colors.white,
+        actions: <Widget>[
+          Align(
+            alignment: Alignment.center,
+            child: Text(
+              "0",
+              style: TextStyle(
+                  color: Colors.grey
+              ),
+            ),
+          ),
+          IconButton(
+              icon: Icon(Icons.star),
+              color: Colors.grey,
+              onPressed: (){},
+          ),
+          IconButton(
+            icon: Icon(Icons.search),
+            color: Colors.grey,
+            onPressed: (){},
+          )
+        ],
+      ),
+      body: Container(
+
+      ),
+    );
   }
 }
